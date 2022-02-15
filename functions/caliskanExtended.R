6aef4f04c659d322859736ded26f5e9e896192e9
AlicjaDobrzeniecka <alizjonizm@gmail.com>
2022-02-15 19:25:11 +0100

    Adds files

--- functions/caliskanExtended.R
+++ functions/caliskanExtended.R
@@ -16,7 +16,7 @@ library(grid)
 
 # X = {t1,t2,t3,t4,t5,t6,t7,t8}, Y = {t9,t10,t11,t12,t13,t14,t15,t16}
 
-set.seed(123)
+set.seed(123456)
 t1 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
 t2 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
 t3 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
@@ -58,5 +58,42 @@ factor <- sd(c(s(t1),s(t2),s(t3),s(t4),s(t5),s(t6),s(t7),s(t8),s(t9),s(t10),s(t1
 rawsd/factor
 numerator <-  mean(s(t1),s(t2),s(t3),s(t4),s(t5),s(t6),s(t7),s(t8)) - mean(s(t9),s(t10),s(t11),s(t12),s(t13),s(t14),s(t15),s(t16))
 print(list(factor = factor,numerator = numerator, bias = numerator / factor))
+
+
+biasesD1 <- numeric(10000)
+
+for(i in 1:10000){
+  t1 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t2 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t3 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t4 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t5 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t6 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t7 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t8 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t9 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t10 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t11 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t12 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t13 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t14 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t15 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t16 <- data.frame(A  = rnorm(8,0,0.05), B = rnorm(8,0,0.05))
+  t1d1 <- data.frame(A  = rnorm(5,.1,0.1), B = rnorm(5,0,0.1))
+  t2d1 <- data.frame(A  = rnorm(5,.1,0.1), B = rnorm(5,0,0.1))
+  t3d1 <- data.frame(A  = rnorm(5,0,0.1), B = rnorm(5,.1,0.1))
+  t4d1 <- data.frame(A  = rnorm(5,0,0.1), B = rnorm(5,.1,0.1))
+  
+  factorD1 <- sd(c(s(t1),s(t2),s(t3),s(t4),s(t5),s(t6),s(t7),s(t8),s(t9),s(t10),s(t11),s(t12),s(t13),s(t14),s(t15),s(t16)))
+  numeratorD1 <-  mean(s(t1),s(t2),s(t3),s(t4),s(t5),s(t6),s(t7),s(t8)) - mean(s(t9),s(t10),s(t11),s(t12),s(t13),s(t14),s(t15),s(t16))
+  biasesD1[i] <- numeratorD1/factorD1
+}
+
+ggplot()+geom_histogram(aes(x=biasesD1, y = ..density..), alpha = 0.6, bins=50)+
+  theme_tufte()+labs(title="10k biases for different means and sd =.05")+ xlab("bias")
+
+
+# TODO: add simulation with realistic standard deviation
+
 
 
--- functions/stdCosineSimilarity.R
+++ functions/stdCosineSimilarity.R
@@ -1,6 +1,12 @@
 # Standard deviation for cosine similarity from Google datasets
 
 
+# read data
 
+control = read.csv("./datasets/controlGoogle.csv")
+race = read.csv("./datasets/raceGoogle.csv")
+religion = read.csv("./datasets/religionGoogle.csv")
+
+# prepare datasets 
 
 
