8f313ad99b04a68a0e899e95465038d28a185859
Rafal <rafal@autoreg-129-10-151-234.nunet.neu.edu>
2022-06-06 12:07:45 -0400

    meh

--- functions/buildModel.R
+++ functions/buildModel.R
@@ -7,27 +7,27 @@ library(grid)
 
 head(weat1Glove)
 
-
-modelResult <- ulam(
-  alist(
-    distance ~ dnorm(mu,sigma),
-    mu <- d[pwi] * different + a[pwi] * associated + h[pwi] * human + n[pwi] * none,
-    d[pwi] ~ dnorm(dbar, dsigmabar),
-    a[pwi] ~ dnorm(abar, asigmabar),
-    h[pwi] ~ dnorm(hbar, hsigmabar),
-    n[pwi] ~ dnorm(nbar, nsigmabar),
-    dbar ~ dnorm(1,.3),
-    abar ~ dnorm(1,.3),
-    hbar ~ dnorm(1,.3),
-    nbar ~ dnorm(1,.3),
-    dsigmabar ~ dexp(2),
-    asigmabar ~ dexp(2),
-    hsigmabar ~ dexp(2),
-    nsigmabar ~ dexp(2),
-    sigma <- s[connection],
-    s[connection] ~  dexp(2)
-  ),   data = weat1Glove, chains=2 , iter=8000 , warmup=1000,  log_lik = TRUE, cores = 4
-)
+# 
+# modelResult <- ulam(
+#   alist(
+#     distance ~ dnorm(mu,sigma),
+#     mu <- d[pwi] * different + a[pwi] * associated + h[pwi] * human + n[pwi] * none,
+#     d[pwi] ~ dnorm(dbar, dsigmabar),
+#     a[pwi] ~ dnorm(abar, asigmabar),
+#     h[pwi] ~ dnorm(hbar, hsigmabar),
+#     n[pwi] ~ dnorm(nbar, nsigmabar),
+#     dbar ~ dnorm(1,.3),
+#     abar ~ dnorm(1,.3),
+#     hbar ~ dnorm(1,.3),
+#     nbar ~ dnorm(1,.3),
+#     dsigmabar ~ dexp(2),
+#     asigmabar ~ dexp(2),
+#     hsigmabar ~ dexp(2),
+#     nsigmabar ~ dexp(2),
+#     sigma <- s[connection],
+#     s[connection] ~  dexp(2)
+#   ),   data = weat1Glove, chains=2 , iter=8000 , warmup=1000,  log_lik = TRUE, cores = 4
+# )
 
 
 
